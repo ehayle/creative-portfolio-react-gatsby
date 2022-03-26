72950c5f1b9bd6e2a6bab6096d67ca877106fd4a
E <pucadesignstudio@gmail.com>
2022-03-26 01:02:44 -0600

    styled

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -24,7 +24,7 @@ import GlobalContext from "../../context/GlobalContext";
 
 import GlobalStyle from "../../utils/globalStyle";
 
-import imgFavicon from "../../assets/favicon.png";
+import imgFavicon from "../../assets/favicon.jpg";
 
 import "../../assets/fonts/icon-font/fonts/avasta.ttf";
 import "../../assets/fonts/icon-font/fonts/Grayic.ttf";
